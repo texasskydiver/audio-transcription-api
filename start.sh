39d0999255a0555bf79df4a466591d32dd07e94a
Howie Stevens <txsqlguy1969@gmail.com>
2025-04-28 08:51:14 -0500

    fix: convert start.sh to unix LF line endings for shell compatibility

--- start.sh
+++ start.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-printenv
-
-echo "PORT is: $PORT"
-if [ -z "$PORT" ]; then
-  echo "PORT environment variable is not set!"
-  exit 1
-fi
-exec uvicorn main:app --host 0.0.0.0 --port $PORT 
+﻿#!/bin/shprintenvecho "PORT is: $PORT"if [ -z "$PORT" ]; then  echo "PORT environment variable is not set!"  exit 1fiexec uvicorn main:app --host 0.0.0.0 --port $PORT 
